116d00b49e8b51189216dacb4993528a41fd8307
Erik Binder <erik.binder@hotmail.com>
2026-01-30 14:49:15 +0200

    RuleSet enum and CogniCryptTestRunner implemented

--- sonar-crypto-plugin/src/test/java/org/sonarcrypto/CogniCryptTestRunner.java
+++ sonar-crypto-plugin/src/test/java/org/sonarcrypto/CogniCryptTestRunner.java
@@ -1,15 +1,28 @@
 package org.sonarcrypto;
 
+import boomerang.scope.Method;
+import boomerang.scope.WrappedClass;
+import com.google.common.collect.Table;
+import crypto.analysis.errors.AbstractError;
 import de.fraunhofer.iem.scanner.HeadlessJavaScanner;
 import org.sonarcrypto.rules.CryslRuleProvider;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Set;
 
 public class CogniCryptTestRunner {
 
+    Table<WrappedClass, Method, Set<AbstractError>> errorCollection;
+
     public void run(String applicationPath, RuleSet rules) throws IOException {
-        CryslRuleProvider provider = new CryslRuleProvider();
-        HeadlessJavaScanner scanner = new HeadlessJavaScanner(applicationPath, Objects.requireNonNull(provider.extractCryslFileToTempDir(rules.dirName)).toString());
+        var provider = new CryslRuleProvider();
+        var scanner = new HeadlessJavaScanner(applicationPath, Objects.requireNonNull(provider.extractCryslFileToTempDir(rules.dirName)).toString());
+        scanner.scan();
+        errorCollection = scanner.getCollectedErrors();
+    }
+
+    public Table<WrappedClass, Method, Set<AbstractError>> getErrorCollection() {
+        return errorCollection;
     }
 }
--- sonar-crypto-plugin/src/test/java/org/sonarcrypto/CryptoSensorTest.java
+++ sonar-crypto-plugin/src/test/java/org/sonarcrypto/CryptoSensorTest.java
@@ -2,6 +2,7 @@ package org.sonarcrypto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -19,5 +20,18 @@ class CryptoSensorTest {
     CryptoSensor sensor = new CryptoSensor();
     sensor.execute(SensorContextTester.create(tempDir));
     assertThat(logTester.logs()).containsExactly("Failed to build Maven project");
+  }
+
+  @Test
+  void example_test () throws IOException {
+      var runner = new CogniCryptTestRunner();
+      runner.run("", RuleSet.BOUNCY_CASTLE);
+      var errors = runner.errorCollection;
+      var errorSet = errors.cellSet();
+
+      for (var wrappedClassMethodSetCell : errorSet) {
+          System.out.println(wrappedClassMethodSetCell);
+      }
+
   }
 }
