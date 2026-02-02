5be9d53c95df99049f4fe98bd2e35ab66a027a66
Erik Binder <erik.binder@hotmail.com>
2026-02-02 16:51:26 +0200

    test coverage

--- sonar-crypto-plugin/src/test/java/org/sonarcrypto/ResourceExtractorTest.java
+++ sonar-crypto-plugin/src/test/java/org/sonarcrypto/ResourceExtractorTest.java
@@ -1,14 +1,34 @@
 package org.sonarcrypto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.sonarcrypto.utils.ResourceExtractor;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ResourceExtractorTest {
 
     @Test
-    void extractTest () {
-        var path = Path.of("crysl_rules");
+    void extractTest () throws IOException {
+        final var ruleset = "bc-jca";
+        final var fileEnding = ".zip";
+        final var rulesFolderName = "crysl_rules";
+        final var tempDir = Files.createTempDirectory(rulesFolderName);
+        final var extractedRulePaths = ResourceExtractor.extract(
+                rulesFolderName,
+                tempDir,
+                fileEnding,
+                ruleset::equalsIgnoreCase
+        );
+
+        final var foundRules = extractedRulePaths.size();
+        final var entry = extractedRulePaths.get(0).getFileName();
+
+        assertThat(foundRules).isNotZero();
+        assertThat(extractedRulePaths).isNotEmpty();
+        assertThat(entry.toString()).endsWith("bc-jca.zip");
     }
 }
