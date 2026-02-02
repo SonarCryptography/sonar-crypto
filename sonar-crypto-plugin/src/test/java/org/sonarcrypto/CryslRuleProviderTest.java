3a08641261ce5448e9b98302e47368743c5310f4
Erik Binder <erik.binder@hotmail.com>
2026-02-02 17:23:21 +0200

    updated

--- sonar-crypto-plugin/src/test/java/org/sonarcrypto/CryslRuleProviderTest.java
+++ sonar-crypto-plugin/src/test/java/org/sonarcrypto/CryslRuleProviderTest.java
@@ -1,6 +1,7 @@
 package org.sonarcrypto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import org.sonarcrypto.rules.CryslRuleProvider;
@@ -17,14 +18,22 @@ public class CryslRuleProviderTest {
         var provider = new CryslRuleProvider();
         var path = provider.extractRulesetToTempDir("bc");
         var myZip = new File(path.toUri());
+        assertThat(myZip.exists());
+        assertThat(myZip.isFile());
         try (var zip = new ZipFile(myZip)) {
             var dirEntry = zip.getEntry("BouncyCastle");
+            assertThat(dirEntry.isDirectory());
             var entry = zip.getEntry("BouncyCastle/RSAEngine.crysl");
             assertThat(entry).as("File should exist").isNotNull();
-            assertThat(dirEntry.isDirectory());
         }
         assertThat(new File(path.toUri()).exists());
         assertThat(new File(path.toUri()).isFile());
         System.out.println(path);
+
+        assertThatThrownBy(() -> {
+            // The code that should crash
+            provider.extractRulesetToTempDir("");
+        })
+                .isInstanceOf(IOException.class);
     }
 }
