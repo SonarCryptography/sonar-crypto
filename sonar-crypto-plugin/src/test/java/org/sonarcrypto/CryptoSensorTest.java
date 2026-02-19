57116290303320c1052adc794c844bfd6793b29e
Jan Martin Persch <jan.persch@iem.fraunhofer.de>
2026-02-19 14:12:08 +0100

    Add `MavenProject` test

--- sonar-crypto-plugin/src/test/java/org/sonarcrypto/CryptoSensorTest.java
+++ sonar-crypto-plugin/src/test/java/org/sonarcrypto/CryptoSensorTest.java
@@ -4,12 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.sonarcrypto.utils.test.asserts.CcErrorsAssert.assertOnFilteredCcErrorsThat;
 
-import crypto.analysis.errors.ConstraintError;
-import crypto.analysis.errors.TypestateError;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 import org.jspecify.annotations.NullMarked;
 import org.junit.jupiter.api.Test;
@@ -18,8 +13,6 @@ import org.junit.jupiter.api.io.TempDir;
 import org.sonar.api.batch.sensor.SensorDescriptor;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.testfixtures.log.LogTesterJUnit5;
-import org.sonarcrypto.utils.cognicrypt.crysl.Ruleset;
-import org.sonarcrypto.utils.test.runner.MavenProjectTestRunner;
 
 @NullMarked
 class CryptoSensorTest {
@@ -48,21 +41,5 @@ class CryptoSensorTest {
 
     assertThat(context.allIssues()).isEmpty();
     assertThat(logTester.logs()).containsExactly("Failed to build Maven project");
-  }
-
-  @Test
-  void mavenProjectTest() throws IOException, URISyntaxException {
-    final var runner = new MavenProjectTestRunner();
-
-    final var collectedErrors =
-        runner.run("../e2e/src/test/resources/Java/Maven/Basic", Ruleset.JCA).collectedErrors();
-
-    assertOnFilteredCcErrorsThat(
-            collectedErrors,
-            "com.example.crypto.WeakCryptoExamples",
-            "byte[] encryptWithDES(byte[])")
-        .anySatisfy(
-            error ->
-                assertThat(error).isInstanceOfAny(ConstraintError.class, TypestateError.class));
   }
 }
