7177df35093a5af6bbf28c7e75c210d1544fa1bb
Patrick Müller <patrick.mueller@tu-darmstadt.de>
2026-05-13 09:30:50 +0200

    Use Disabled instead of Ignore

--- e2e/src/test/java/org/sonarcrypto/e2e/JavaAndJimpleAnalysisResultsTest.java
+++ e2e/src/test/java/org/sonarcrypto/e2e/JavaAndJimpleAnalysisResultsTest.java
@@ -2,7 +2,7 @@ package org.sonarcrypto.e2e;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.sonarcrypto.utils.cognicrypt.crysl.Ruleset;
 import org.sonarcrypto.utils.test.asserts.CcErrorsAssert;
@@ -20,7 +20,8 @@ public class JavaAndJimpleAnalysisResultsTest {
     assertResultsEqual(Ruleset.BC);
   }
 
-  @Ignore(
+  @Test
+  @Disabled(
       "Tink cognicrypt analyses fail since boomerang calls Class.forName This test should be re-enabled once the issue is resolved.")
   void java_and_jimple_results_match_for_tink() throws IOException, URISyntaxException {
     assertResultsEqual(Ruleset.TINK);
