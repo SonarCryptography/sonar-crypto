40e555d46dcd4b3f3fb209548956780457e503e7
Jan Martin Persch <jan.persch@iem.fraunhofer.de>
2026-08-04 13:28:01 +0200

    Set location with precise argument position

--- utils/cognicrypt/src/main/java/org/sonarcrypto/utils/cognicrypt/crysl/ConverterUtils.java
+++ utils/cognicrypt/src/main/java/org/sonarcrypto/utils/cognicrypt/crysl/ConverterUtils.java
@@ -10,6 +10,7 @@ import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.TextRange;
+import sootup.core.jimple.basic.NoPositionInformation;
 import sootup.core.jimple.basic.SimpleStmtPositionInfo;
 import sootup.core.jimple.basic.StmtPositionInfo;
 import sootup.core.model.Position;
@@ -21,20 +22,22 @@ public class ConverterUtils {
     // Utility class
   }
 
-  /**
-   * Selects a location as precise as possible, if the {@link AbstractError#getErrorStatement()
-   * error's statement} is as {@link JimpleUpStatement}. Otherwise, it simply uses the {@link
-   * AbstractError#getLineNumber() error's line number}.
-   */
   public static TextRange selectLocation(InputFile inputFile, StmtPositionInfo stmtPosition) {
-    return selectLocation(inputFile, stmtPosition.getStmtPosition());
+    return selectLocation(inputFile, stmtPosition, null);
   }
 
-  /**
-   * Selects a location as precise as possible, if the {@link AbstractError#getErrorStatement()
-   * error's statement} is as {@link JimpleUpStatement}. Otherwise, it simply uses the {@link
-   * AbstractError#getLineNumber() error's line number}.
-   */
+  public static TextRange selectLocation(
+      InputFile inputFile, StmtPositionInfo stmtPosition, @Nullable CallInfo callInfo) {
+    final var calleeInfo = callInfo != null ? callInfo.calleeInfo() : null;
+    Position argumentPosition =
+        stmtPosition.getOperandPosition(calleeInfo != null ? callInfo.argumentIndex() + 1 : 0);
+    return selectLocation(
+        inputFile,
+        argumentPosition == null || NoPositionInformation.getInstance().equals(argumentPosition)
+            ? stmtPosition.getStmtPosition()
+            : argumentPosition);
+  }
+
   public static TextRange selectLocation(InputFile inputFile, Position position) {
 
     final var startLine = max(position.getFirstLine(), 1);
@@ -43,23 +46,25 @@ public class ConverterUtils {
     final var endLine = position.getLastLine();
     var endLineOffset = position.getLastCol();
 
-    try {
-      final var actualLine =
-          inputFile.contents().lines().skip(max(0, startLine - 1)).findFirst().orElse("");
+    if (startLineOffset <= 0) {
+      try {
+        final var actualLine =
+            inputFile.contents().lines().skip(max(0, startLine - 1)).findFirst().orElse("");
 
-      if (!actualLine.isEmpty()) {
-        final var actualLineOffset =
-            (int)
-                min(
-                    actualLine.chars().takeWhile(Character::isWhitespace).count(),
-                    Integer.MAX_VALUE);
+        if (!actualLine.isEmpty()) {
+          final var actualLineOffset =
+              (int)
+                  min(
+                      actualLine.chars().takeWhile(Character::isWhitespace).count(),
+                      Integer.MAX_VALUE);
 
-        if (actualLineOffset < actualLine.length() - 1) startLineOffset = actualLineOffset;
+          if (actualLineOffset < actualLine.length() - 1) startLineOffset = actualLineOffset;
 
-        if (endLineOffset < 1) endLineOffset = actualLine.length();
+          if (endLineOffset < 1) endLineOffset = actualLine.length();
+        }
+      } catch (IOException e) {
+        // Ignore and continue.
       }
-    } catch (IOException e) {
-      // Ignore and continue.
     }
 
     return inputFile.newRange(
